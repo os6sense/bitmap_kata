09d9665a172afbb5a4db4d706c4f6c0e0edbcd3e
os6sense <leej@sowhatresearch.com>
2015-08-02 21:19:50 +0100

    Additional specs for x and y coords

--- spec/coords_spec.rb
+++ spec/coords_spec.rb
@@ -1,23 +1,22 @@
-
 require_relative '../lib/coord.rb'
 
 describe Coord do
   subject { described_class.new(x, y) }
-  describe '#initialize' do
-    let(:y) { 0 }
-    let(:x) { 0 }
+  let(:y) { 10 }
+  let(:x) { 10 }
 
+  describe '#initialize' do
     context 'when x is within the default bounds' do
       it { is_expected.to be_a described_class }
       it 'sets x to the provided value' do
-        expect(subject.x).to eq 0
+        expect(subject.x).to eq x
       end
     end
 
     context 'when y is within the default bounds' do
       it { is_expected.to be_a described_class }
       it 'sets y to the provided value' do
-        expect(subject.y).to eq 0
+        expect(subject.y).to eq y
       end
     end
 
@@ -47,6 +46,36 @@ describe Coord do
       it 'raises an ArgumentError' do
         expect { subject }.to raise_error ArgumentError
       end
+    end
+  end
+
+  describe '#x' do
+    subject { described_class.new(x, y).x }
+    it { is_expected.to eq x }
+  end
+
+  describe '#y' do
+    subject { described_class.new(x, y).y }
+    it { is_expected.to eq y }
+  end
+
+  describe '#y=' do
+    let(:new_y) { 20 }
+    it 'sets y to the provided value' do
+      expect(subject.y).not_to eq new_y # sanity check
+
+      subject.y = y
+      expect(subject.y).to eq y
+    end
+  end
+
+  describe '#x=' do
+    let(:new_x) { 20 }
+    it 'sets x to the provided value' do
+      expect(subject.x).not_to eq new_x # sanity check
+
+      subject.x = x
+      expect(subject.x).to eq x
     end
   end
 end
