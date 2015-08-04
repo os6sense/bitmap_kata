b0987682cd5a178982d1be927bbcd029373625e1
os6sense <leej@sowhatresearch.com>
2015-08-04 22:49:05 +0100

    Coord - Refactored out redundant code, added to_s method

--- lib/coord.rb
+++ lib/coord.rb
@@ -4,8 +4,8 @@
 # range min..max will result an an argument error. Default bounds of 0 to 250
 # are provided.
 class Coord
-  attr_reader :x,
-              :y
+  attr_accessor :x,
+                :y
 
   attr_reader :xmin,
               :xmax
@@ -13,7 +13,11 @@ class Coord
   attr_reader :ymin,
               :ymax
 
-  def initialize(x, y, xmin: 1, xmax: 250, ymin: 1, ymax: 250)
+  # Create a new coordinate pair; x an y are required but minimum and
+  # maximum bounts can be set by using the named parameters xmin, xmax, ymin
+  # and ymax
+  def initialize(x, y, xmin: 1, xmax: 250,
+                       ymin: 1, ymax: 250)
     @xmin, @xmax = xmin, xmax
     @ymin, @ymax = ymin, ymax
 
@@ -21,26 +25,19 @@ class Coord
     self.y = y
   end
 
-  def x=(val)
-    @x = val
-  end
-
-  def y=(val)
-    @y = val
-  end
-
+  # returns true if both x and y are within the bounds of x and y min/max
   def valid?
     test_bounds(x, @xmin, @xmax) && test_bounds(y, @ymin, @ymax)
+  end
+
+  def to_s
+    "[#{x}, #{y}]"
   end
 
   private
 
   # check if val is valid
   def test_bounds(val, min, max)
-    if val >= min && val <= max
-      true
-    else
-      false
-    end
+    val >= min && val <= max
   end
 end
