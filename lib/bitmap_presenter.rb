70259ebfe7369eaebcee4d26c7d5ae8f9a4c6ebf
os6sense <leej@sowhatresearch.com>
2015-08-04 22:45:58 +0100

    Improved documentation

--- lib/bitmap_presenter.rb
+++ lib/bitmap_presenter.rb
@@ -1,5 +1,10 @@
-# SImple presenter to output a bitmap
+# Simple presenter to output a bitmap. The +show+ method is expected to be
+# called to output the bitmap passed as the first parameter of that method.
 class BitmapPresenter
+  # == PARAMS
+  # +bitmap+::    - the bitmap to print.
+  # +out:+::      - optional, the output IO object to write to.
+  #                 (default: $stdout)
   def show(bitmap, out: $stdout)
     bitmap.each do |row|
       row.each do |col|
