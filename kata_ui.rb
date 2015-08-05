05ca26aa1c1b6d93283245e9de6e8e75bd606522
os6sense <leej@sowhatresearch.com>
2015-08-05 21:33:43 +0100

    input is now checked stripped and check not to be blank

--- kata_ui.rb
+++ kata_ui.rb
@@ -3,11 +3,12 @@ require_relative 'lib/bitmap.rb'
 
 bitmap = Bitmap
 
+puts "Please first create a new bitmap with the I command\n"
 loop do
   print '> '
-  input = gets
+  input = gets.strip
   begin
-    bitmap = CommandParser.parse(input).apply(bitmap)
+    bitmap = CommandParser.parse(input).apply(bitmap) if input != ''
   rescue RuntimeError => e
     puts e.message
   end
